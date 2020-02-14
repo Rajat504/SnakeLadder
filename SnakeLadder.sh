42302ae9d6e6ab922e34e2fde982a02a9538a358
Rajat504 <rajatravindar504@gmail.com>
2020-02-14 18:28:58 +0530

    [Rajat]added snakeladder diagram

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,2 +1,43 @@
-#!/bin/sh
-echo "Welcome to SnakeLadder Game"
+board()
+{
+a=101
+declare -A arr
+	for((i=0;i<10;i++))
+	do
+		if(($i%2==0))
+		then
+			for((j=0;j<10;j++))
+			do
+				a=$(($a-1))
+				arr[$i,$j]=$a
+			done
+		else
+			for((j=9;j>=0;j--))
+			do
+				a=$(($a-1))
+				arr[$i,$j]=$a
+			done
+		fi
+	done
+			echo "_________________________________________________"
+				for((i=0;i<10;i++))
+				do
+					if(($i==9))
+					then
+						for((j=0;j<10;j++))
+						do
+							echo -n "|  "
+							echo -n "$((arr[$i,$j])) "
+						done
+					else
+						for((j=0; j<10; j++))
+						do
+							echo -n "| "
+							echo -n "$((arr[$i,$j])) "
+						done
+					fi
+					echo " "
+					echo "__________________________________________________"
+				done
+}
+board
